8c215adae3bc41cf3858cdb82a596b2b40e50014
facelessagony <eric.solomon.ca@gmail.com>
2022-11-13 22:46:15 +0000

    tweet-hover.js added

---
+++ public/scripts/tweet-hover.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  $("section.tweet").hover(function () {
+    //handlerIn event
+    this.style["box-shadow"] = "5px 5px #9d9d9d";
+  }, function () {
+    //handlerOut event
+    this.style["box-shadow"] = "";
+  })
+  
+  $("i.fa-flag").hover(function () {
+    //handlerIn event
+    console.log("console log this: ", this)
+    this.style["color"] = "#0878af";
+  }, function () {
+    //handlerOut event
+    this.style["color"] = "";
+  })
+
+  $("i.fa-share").hover(function () {
+    //handlerIn event
+    console.log("console log this: ", this)
+    this.style["color"] = "#0878af";
+  }, function () {
+    //handlerOut event
+    this.style["color"] = "";
+  })
+
+  $("i.fa-heart").hover(function () {
+    //handlerIn event
+    console.log("console log this: ", this)
+    this.style["color"] = "#0878af";
+  }, function () {
+    //handlerOut event
+    this.style["color"] = "";
+  })
+
+})
