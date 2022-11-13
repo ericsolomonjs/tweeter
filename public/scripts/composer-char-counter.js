248e892563e49658519afa46867269972defb826
facelessagony <eric.solomon.ca@gmail.com>
2022-11-13 22:45:27 +0000

    char counter script added

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,30 @@
+function setCounter(name, value) {
+  let target = document.getElementsByClassName(name);
+  target[name].value = value;
+  console.log(target);
+  if (target[name].value < 0) {
+    target[name].style.color = "red";
+  } else {
+    target[name].style.color = "";
+  }
+}
+
+function getValueById (name) {
+  return document.getElementById(name).value;
+}
+
+function updateValue () {
+  const txtLength = this.value.length ;
+  setCounter('counter', 140 - txtLength);
+};
+
+//on document ready
+$(document).ready(function () {
+  console.log("the document is ready to be altered")
+  const input = document.getElementById('tweet-text');
+  input.addEventListener('input', updateValue);
+})
+
+
+
+
