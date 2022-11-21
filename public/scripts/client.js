4d52f70ea1a86a20d7868bf5b5ab3caa548a8052
facelessagony <eric.solomon.ca@gmail.com>
2022-11-21 16:17:14 +0000

    move time since string feature to own file and import in index.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,31 +8,10 @@ const createTweetElement = function (object) {
   const timeSince = Date.now() - object.created_at;
 
   let fnArticle = '';
-  let monthsSince = Math.trunc(timeSince / 1000 / 60 / 60 / 24 / 30);
-  let daysSince = Math.trunc(timeSince / 1000 / 60 / 60 / 24);
-  let hoursSince = Math.trunc(timeSince / 1000 / 60 / 60);
-  let minutesSince = Math.trunc(timeSince / 1000 / 60);
-  let secondsSince = Math.trunc(timeSince / 1000);
 
-  let timeString = '';
-  //choose appropriate
-  if (monthsSince > 0) {
-    timeString = `Posted ${monthsSince} months ago.`;
-  } else
-  if (daysSince > 0) {
-    timeString = `Posted ${daysSince} days ago.`;
-  } else
-  if (hoursSince > 0) {
-    timeString = `Posted ${hoursSince} hours ago.`;
-  } else
-  if (minutesSince > 0) {
-    timeString = `Posted ${minutesSince} minutes ago.`;
-  } else
-  if (secondsSince > 0) {
-    timeString = `Posted ${secondsSince} seconds ago.`;
-  } else {
-    timeString = `Posted 0 seconds ago.`;
-  }
+  //call getTimeString to create the time since posted string
+  const timeString = getTimeString(timeSince);
+  
 
   if (object.user.avatars == '') {
     object.user.avatars = "https://i.imgur.com/nlhLi3I.png";
